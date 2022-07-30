1e603b5807238b537793845d26d3c585c03c2b84
cburgyan <cburgyan@gmail.com>
2022-07-30 17:27:27 -0500

    clean up code and comments

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import wtforms
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -8,11 +7,15 @@ import csv
 import datetime
 
 
+# Create Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+# Add Bootstrap
 Bootstrap(app)
 
 
+# Create Form
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
     location = URLField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
@@ -22,14 +25,6 @@ class CafeForm(FlaskForm):
     wifi_strength = SelectField('Wifi Strength Rating', validators=[DataRequired()], choices=['✘', '💪', '💪💪', '💪💪💪', '💪💪💪💪', '💪💪💪💪💪'])
     available_power = SelectField('Power Socket Availability', validators=[DataRequired()], choices=['✘', '🔌', '🔌🔌', '🔌🔌🔌', '🔌🔌🔌🔌', '🔌🔌🔌🔌🔌'])
     submit = SubmitField('Submit')
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
 
 
 # all Flask routes below
@@ -58,11 +53,8 @@ def add_cafe():
         #Save to file
         with open('cafe-data.csv', 'a', encoding='utf8') as csv_file:
             csv_file.write(f'{form.cafe.data},{form.location.data},{open_time},{close_time},{form.coffee_rating.data},{form.wifi_strength.data},{form.available_power.data}\n')
-        return redirect(url_for('cafes'))
 
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
 
 
